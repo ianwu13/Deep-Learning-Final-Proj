ac4f638bfdc1b4f6e6c7353f94c0ea9ff0862e98
ianwu13 <ianwu13@me.com>
2022-05-02 18:41:31 -0400

    added resnet

---
+++ models/resnet.py
@@ -0,0 +1,124 @@
+import torch
+import torch.nn as nn
+
+# Adapted from torchvision implementation of resnet
+
+
+class resblock(nn.Module):
+    def __init__(self, inplanes, planes, stride = 1, downsample=None, groups = 1, base_width = 64, dilation = 1):
+        super().__init__()
+        norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, groups=1, bias=False, dilation=1)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, groups=1, bias=False, dilation=1)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class resnet34(nn.Module):
+    def __init__(self, num_classes = 2, groups = 1, width_per_group = 64):
+        super().__init__()
+        norm_layer = nn.BatchNorm2d
+        self._norm_layer = nn.BatchNorm2d
+
+        self.inplanes = 64
+        self.dilation = 1
+        # each element in the tuple indicates if we should replace
+        # the 2x2 stride with a dilated convolution instead
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(64, 3)
+        self.layer2 = self._make_layer(128, 4, stride=2, dilate=False)
+        self.layer3 = self._make_layer(256, 6, stride=2, dilate=False)
+        self.layer4 = self._make_layer(512, 3, stride=2, dilate=False)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, planes, blocks, stride = 1, dilate = False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride, bias=False),
+                norm_layer(planes),
+            )
+
+        layers = []
+        layers.append(
+            resblock(
+                self.inplanes, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer
+            )
+        )
+        self.inplanes = planes
+        for _ in range(1, blocks):
+            layers.append(
+                resblock(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                )
+            )
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
--- models/vggnet.py
+++ models/vggnet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class vggnet16(nn.Module):
-    def __init__(self, num_classes=2):
+    def __init__(self, num_classes=2, more_conv=False, more_fc=False):
         super(vggnet16, self).__init__()
 
         self.features = nn.Sequential(
@@ -36,71 +36,28 @@ class vggnet16(nn.Module):
 
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
 
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Linear(4096, num_classes),
-            nn.Softmax(dim=1)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-
-        return x
-
-
-class vggnet16_more_fc_layers(nn.Module):
-    def __init__(self, num_classes=2):
-        super(vggnet16, self).__init__()
-
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=2, stride=2), 
-            nn.Conv2d(64, 256, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=2, stride=2), 
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=2, stride=2), 
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=2, stride=2), 
-        )
-
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
-
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Linear(4096, 1000),
-            nn.ReLU(True),
-            nn.Linear(1000, 1000),
-            nn.ReLU(True),
-            nn.Linear(1000, num_classes),
-            nn.Softmax(dim=1)
-        )
+        if not more_fc:
+            self.classifier = nn.Sequential(
+                nn.Linear(512 * 7 * 7, 4096),
+                nn.ReLU(True),
+                nn.Linear(4096, 4096),
+                nn.ReLU(True),
+                nn.Linear(4096, num_classes),
+                nn.Softmax(dim=1)
+            )
+        else:
+            self.classifier = nn.Sequential(
+                nn.Linear(512 * 7 * 7, 4096),
+                nn.ReLU(True),
+                nn.Linear(4096, 4096),
+                nn.ReLU(True),
+                nn.Linear(4096, 1000),
+                nn.ReLU(True),
+                nn.Linear(1000, 1000),
+                nn.ReLU(True),
+                nn.Linear(1000, num_classes),
+                nn.Softmax(dim=1)
+            )
 
     def forward(self, x):
         x = self.features(x)
